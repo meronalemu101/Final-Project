35d641198da07d9a101ad98421797ea622e810e9
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-12-14 20:06:49 -0700

    Update StimuliFolder.py

--- StimuliFolder.py
+++ StimuliFolder.py
@@ -1,3 +1,3 @@
-# Save as: [cupA.png](https://drive.google.com/file/d/145f6rMt121iBlAi-mRGATXO5T6hPnQZV/view?usp=share_link)
-# Save as: [cupB.png](https://drive.google.com/file/d/145f6rMt121iBlAi-mRGATXO5T6hPnQZV/view?usp=share_link)
-# Save as: [cupB.png](https://drive.google.com/file/d/1hMA6TH213zXCT1_2Zuj9Kgz5wWzRmTPE/view?usp=share_link)
+# Save as cupA.png: (https://drive.google.com/file/d/145f6rMt121iBlAi-mRGATXO5T6hPnQZV/view?usp=share_link)
+# Save as cupB.png: (https://drive.google.com/file/d/145f6rMt121iBlAi-mRGATXO5T6hPnQZV/view?usp=share_link)
+# Save as cupC.png: (https://drive.google.com/file/d/1hMA6TH213zXCT1_2Zuj9Kgz5wWzRmTPE/view?usp=share_link)
